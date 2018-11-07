303642e3f121b084d4c3974989f66555d7a41885
Pavel Shumeika <pavel.shumeika@intellectsoft.net>
2018-11-08 00:01:27 +0300

    Logging and refactoring

--- bot.rb
+++ bot.rb
@@ -3,14 +3,20 @@ require_relative 'worker'
 require 'pry'
 
 token = ENV['BOT_TOKEN']
+puts 'Running capybara worker'
 worker = Worker.new
 
 Telegram::Bot::Client.run(token) do |bot|
   bot.listen do |message|
     if message.text == '/lobby'
-      url = worker.create_lobby
-      bot.api.send_message(chat_id: message.chat.id, text: url)
-      worker.wait_for_other_players
+      puts "\nCreating lobby"
+      link = worker.create_lobby
+      puts "Lobby: #{link}"
+      bot.api.send_message(chat_id: message.chat.id, text: link)
+      puts 'Waiting for other players'
+      worker.wait_for_other_players ? puts('Player joined') : puts('Nobody joined the game')
+      puts 'Leaving the lobby'
+      worker.leave_lobby
     end
   end
 end
--- worker.rb
+++ worker.rb
@@ -1,34 +1,51 @@
 require 'capybara'
-require_relative 'chrome_headless'
+# require_relative 'chrome_headless'
 
 class Worker
   def initialize
-    @session = Capybara::Session.new(:selenium_chrome_headless).tap do |session|
-      session.visit 'http://surviv.io/'
-      puts 'Running capybara worker'
-    end
+    @session = Capybara::Session.new(:selenium_chrome)
+    @session.visit 'http://surviv.io/'
   end
 
   def create_lobby
-    puts 'Creating lobby'
-    @session.find('#btn-create-team').click
-    @session.find('#btn-team-fill-none').click
-    @session.find('#team-url').text
+    create_team
+    fill_none
+    extract_team_url
   end
 
   def wait_for_other_players
-    puts 'Waiting for other players'
     time_to_wait = 60
+    delay = 2
 
-    while time_to_wait > 0
-      sleep 2
-      time_to_wait -= 2
-      unless @session.find('#team-menu-member-list .team-menu-member:nth-child(2) .name').text.empty?
-        time_to_wait = 0
-      end
+    (time_to_wait / delay).times do
+      return true if other_player_joined?
+
+      sleep delay
     end
 
-    puts 'Leaving the lobby'
+    false
+  end
+
+  def leave_lobby
     @session.find('#btn-team-leave').click
+  end
+
+  private
+
+  def create_team
+    @session.find('#btn-create-team').click
+  end
+
+  def fill_none
+    @session.find('#btn-team-fill-none').click
+  end
+
+  def extract_team_url
+    @session.find('#team-url').text
+  end
+
+  def other_player_joined?
+    other_player = @session.find('#team-menu-member-list .team-menu-member:nth-child(2) .name').text
+    !other_player.empty?
   end
 end
