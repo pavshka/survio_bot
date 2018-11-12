e96c78b42d850989f33c69fa63ca4a5a2b498618
Pavel Shumeika <pavel.shumeika@intellectsoft.net>
2018-11-12 22:31:24 +0300

    Fast fix and multithreading. Needs refactoring

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
 source 'https://rubygems.org'
 
+gem 'eventmachine'
+gem 'faye-websocket'
 gem 'pry'
 gem 'telegram-bot-ruby'
-gem 'faye-websocket'
-gem 'eventmachine'
--- bot.rb
+++ bot.rb
@@ -5,13 +5,15 @@ token = ENV['BOT_TOKEN']
 puts 'Bot started listening'
 Telegram::Bot::Client.run(token) do |bot|
   bot.listen do |message|
-    if message.text == '/lobby'
+    command = message.text.split('@').first
+
+    if command == '/lobby'
       puts 'Received /lobby message'
-      Worker.new(bot, message)
-    elsif message.text == '/arduino'
+      Thread.new { Worker.new(bot, message) }
+    elsif command == '/arduino'
       puts 'Received /arduino message'
       bot.api.send_message(chat_id: message.chat.id, text: 'Pretending to be an Arduino *click*')
-      Worker.new(bot, message)
+      Thread.new { Worker.new(bot, message) }
     else
       puts "Received unsupportd #{message.text}"
     end
