a45d2c47a4cb390f4019a044f9f5d8ac0732cfff
Pavel Shumeika <pavel.shumeika@intellectsoft.net>
2018-11-12 23:01:34 +0300

    Error handling fast fix

--- bot.rb
+++ bot.rb
@@ -3,19 +3,27 @@ require_relative 'worker'
 
 token = ENV['BOT_TOKEN']
 puts 'Bot started listening'
+
 Telegram::Bot::Client.run(token) do |bot|
   bot.listen do |message|
-    command = message.text.split('@').first
+    begin
+      command = message.text.split('@').first
 
-    if command == '/lobby'
-      puts 'Received /lobby message'
-      Thread.new { Worker.new(bot, message) }
-    elsif command == '/arduino'
-      puts 'Received /arduino message'
-      bot.api.send_message(chat_id: message.chat.id, text: 'Pretending to be an Arduino *click*')
-      Thread.new { Worker.new(bot, message) }
-    else
-      puts "Received unsupportd #{message.text}"
+      if command == '/lobby'
+        puts 'Received /lobby message'
+        Thread.new { Worker.new(bot, message) }
+      elsif command == '/arduino'
+        puts 'Received /arduino message'
+        bot.api.send_message(chat_id: message.chat.id, text: 'Pretending to be an Arduino *click*')
+        Thread.new { Worker.new(bot, message) }
+      else
+        puts "Received unsupportd #{message.text}"
+      end
+
+    rescue => e
+      puts 'Error was raised:'
+      puts e
+      next
     end
   end
 end
